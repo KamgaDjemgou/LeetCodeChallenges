055d0e49f0d402ff7b14f44b5c3c40d7cf411dbc
Hisdaele KAMGA <61245640+KamgaDjemgou@users.noreply.github.com>
2025-05-24 15:51:03 +0100

    1071: Solve Greatest Common Divisor of Strings

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-import mergeStringsAlternately.Solution;
+import stringGreatestCommonDivisor.Solution;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.mergeAlternately("125", "ab"));
+        System.out.println(solution.gcdOfStrings("ABCABC", "ABCABC"));
     }
 }
---
+++ src/stringGreatestCommonDivisor/Solution.java
@@ -0,0 +1,15 @@
+package stringGreatestCommonDivisor;
+
+public class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        String gcd = str1.length() > str2.length() ? str2 : str1;
+        while (!gcd.isEmpty() && (cannotDivide(str1, gcd) || cannotDivide(str2, gcd))) {
+            gcd = gcd.substring(0, gcd.length() - 1);
+        }
+        return gcd;
+    }
+
+    public static boolean cannotDivide(String dividend, String divisor) {
+        return !dividend.matches(String.format("(%s)+", divisor));
+    }
+}
